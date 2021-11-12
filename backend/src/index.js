5b12a21b07bc2e0780fdc59b0969e5d7787c6a57
wirsodev <wilson.wmarques@gmail.com>
2021-11-12 16:18:57 +0000

    teste db - create /all route

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,29 @@
 const express = require('express')
 const app = express()
+const db = require('./db/db')
 
 
-app.get('/', (rep, res)=>{
-    res.json({
-        "status":"Hello World"
+// server config
+app.use(express.json())
+
+
+// get data
+app.get('/all', (req, res) => {
+
+    // select all itens from db
+    db.all(`SELECT * FROM properties`, function(err, rows){
+        if(err){
+            console.log(err)
+            return res.send('404')
+        }
+        console.log(rows)
+        return res.send('Send!')
     })
 })
 
 
+
+
 // Server connection
 let port = 3000
-app.listen(process.env.PORT || port, ()=>{console.log('Server up at port - ' + port)})
+app.listen(process.env.PORT || port, () => { console.log('Server up at port - ' + port) })

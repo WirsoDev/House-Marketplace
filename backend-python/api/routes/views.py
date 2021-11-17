3de84d7563e92c1f0c6585c071b45e7efbd2231e
wirsodev <wilson.wmarques@gmail.com>
2021-11-17 14:32:57 +0000

    add /add & /remove routes

--- backend-python/api/routes/views.py
+++ backend-python/api/routes/views.py
@@ -1,5 +1,5 @@
-from api import app
-from flask import Blueprint, jsonify, abort
+from api import app, db
+from flask import Blueprint, jsonify, abort, request
 from api.models import Properties
 from .errors import rise_error
 
@@ -15,11 +15,11 @@ def root():
 @app.route('/all', methods=['GET'])
 def all():
     props = Properties.query.all()
-    props = False
     if props:
         data = []
         for i in props:
             out = {}
+            out['id'] = i.id
             out['name'] = i.name
             out['units'] = i.units
             out['img'] = i.img
@@ -27,5 +27,36 @@ def all():
         return jsonify(data)
     else:
         return rise_error(500, 'Not able to get data')
+
+
+@app.route('/add', methods=['POST'])
+def add():
+    data = request.get_json()
+    if(data):
+        new_propertie = Properties(
+            name = data['name'],
+            units = data['units'],
+            img = data['img']
+        )
+
+        db.session.add(new_propertie)
+        db.session.commit()
+        return jsonify({"message":f"Propertie {data['name']} added!"})
+    else:
+        return rise_error(404, 'Need body data')
+
+
+@app.route('/remove/<id>', methods=['GET'])
+def remove(id):
+    if id:
+        id_exists = Properties.query.filter_by(id=id).first()
+        if id_exists:
+            db.session.delete(id_exists)
+            db.session.commit()
+            return jsonify({"message":f"Removed item with id: {id}"})
+        else:
+            return jsonify({"message":f"No propertie with id: {id}"})
+    return jsonify({"message":"ID required"})
+
 
 
